d09a562db45a75ee9a5e6ea0c6efb64bd8789e94
Nicholas Kohrs <nicholaskohrs06@gmail.com>
2024-03-22 08:15:53 -0400

    Changed the spacing of the inputting number lines. Changed the third number to input n3. Changed the average function to add the three values before dividing.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,29 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+float average(int, int, int);
 
 int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
-	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "First number: ";
+	cin >> n1;
+	cout << "Second number: ";
+	cin >> n2;
+	cout << "Third number: ";
+	cin >> n3;
 
 	float a = average(n1, n2, n3);
 
 	cout << setprecision(1) << fixed << "The average is " << a << endl;
-
+	cin.ignore();
+	cin.get();
 	return 0;
+}
+
+float average(int i1, int i2, int i3)
+{
+	float a = (i1 + i2 + i3) / 3.0;
+	return a;
 }
